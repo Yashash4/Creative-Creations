81e9b46969fc4a7402a96135f4614231170fd3e7
Yashash Sheshagiri <yashashsheshagiri2005@gmail.com>
2025-04-13 00:18:09 +0530

    final3

--- CCCC/client/src/index.js
+++ CCCC/client/src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom/client"; // Updated version of ReactDOM for React 18+
 import App from "./App";
-import "./index.css";
+import "./index.css"; // Global styles
 
+// Create a root for React to mount on
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Render the App component to the root element
 root.render(<App />);
